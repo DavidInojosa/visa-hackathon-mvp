7989214ada8f884ce08049c45aabdd0025a5f6fc
David Inojosa <david.inojosacc@gmail.com>
2022-11-14 09:47:28 -0300

    (feature) Home

---
+++ src/screens/Dashboard/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+import * as S from './styles';
+
+export default function Home() {
+
+  return (
+    <Container>
+
+    </Container>
+  );
+}
