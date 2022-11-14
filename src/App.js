d8d5ea8347d433013d62aac7352b92c318526200
David Inojosa <david.inojosacc@gmail.com>
2022-11-14 09:45:18 -0300

    (config) Configuration files

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { MagicModalPortal } from 'react-native-magic-modal';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Routes from './routes';
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
+
+const App = () => {
+
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    // LOADING SCREEN
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <MagicModalPortal />
+      <Routes />
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
